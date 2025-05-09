edf971b0239a4d9115d6249fb0cb4c6100a0fcaf
Christian Schmeisser <lambretta@live.de>
2025-05-09 17:56:25 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
+//
+//title:    sleeptimer
+//author:   Christian Schmeisser
+//data:     09.05.2025
+//
+
+#define VERSION_INFO "0.1"
+
+#include <iostream>
 #include <QApplication>
 #include <QPushButton>
 
+using std::cout, std::endl;
+
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+    cout << "starting sleeptimer " << VERSION_INFO << endl;
+
+    QApplication app(argc, argv);
     QPushButton button("Hello world!", nullptr);
+
     button.resize(200, 100);
     button.show();
+
     return QApplication::exec();
 }
