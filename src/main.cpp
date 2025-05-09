7c4a25d40b7d986dc0b569308e6f4358b25966d9
Christian Schmeisser <lambretta@live.de>
2025-05-09 18:30:59 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 //
-//title:    sleeptimer
-//author:   Christian Schmeisser
-//data:     09.05.2025
+//title:        sleeptimer
+//author:       Christian Schmeisser
+//date:         09.05.2025
+//description:  A simple desktop app to set, start and stop a shutdown timer in windows.
+//              The timer runs entirely within the app and shutdown is only provoked
+//              when the timer ends. So no shutdown when the app is quit.
 //
 
 #define VERSION_INFO "0.1"
@@ -9,6 +12,7 @@
 #include <iostream>
 #include <QApplication>
 #include <QPushButton>
+#include <QLineEdit>
 
 using std::cout, std::endl;
 
@@ -16,10 +20,13 @@ int main(int argc, char *argv[]) {
     cout << "starting sleeptimer " << VERSION_INFO << endl;
 
     QApplication app(argc, argv);
-    QPushButton button("Hello world!", nullptr);
 
-    button.resize(200, 100);
-    button.show();
+    QPushButton button_startstop("Start timer", nullptr);
+    QLineEdit edit_timer("00:00:00", nullptr);
+
+    //edit_timer.show();
+    button_startstop.resize(100, 50);
+    button_startstop.show();
 
     return QApplication::exec();
 }
