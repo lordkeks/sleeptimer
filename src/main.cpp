95e45d25777b5c75b3905b433f570561661da193
Christian Schmeisser <lambretta@live.de>
2025-05-10 12:22:52 +0200

    Release V1.0.4

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 //              The timer runs entirely within the app and shutdown is only provoked
 //              when the timer ends. So no shutdown when the app is quit.
 
-#define VERSION_INFO "1.0.3" //released 10.05.2025
+#define VERSION_INFO "1.0.4" //released 10.05.2025
 
 #include <iostream>
 #include <QApplication>
